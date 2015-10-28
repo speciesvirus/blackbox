ab3a84fa47d8d1e937d4f71d423d86f2c92b9b6d
jirayut <jirayut.kha@awecent.com>
2015-10-28 10:23:36 +0700

    bootstrap update libs

--- Awecent.Back.Serial/App_Start/BundleConfig.cs
+++ Awecent.Back.Serial/App_Start/BundleConfig.cs
@@ -25,7 +25,6 @@ namespace Awecent.Back.Serial
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                     
                       "~/Content/Spiner.css"));
         }
     }
